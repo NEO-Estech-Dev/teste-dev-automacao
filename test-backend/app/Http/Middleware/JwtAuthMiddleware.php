c4a2084ef6edc7229dd1401b36a2143c9e718faf
Vitor Rocha <deus.1928@gmail.com>
2025-06-03 23:18:46 -0300

    feat: implement JwtAuthMiddleware for JWT token validation and user authentication

---
+++ test-backend/app/Http/Middleware/JwtAuthMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use App\Services\JwtService;
+use Closure;
+use Illuminate\Http\Request;
+use Lcobucci\JWT\UnencryptedToken;
+use Symfony\Component\HttpFoundation\Response;
+
+class JwtAuthMiddleware
+{
+    protected JwtService $jwtService;
+
+    public function __construct(JwtService $jwtService)
+    {
+        $this->jwtService = $jwtService;
+    }
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!$request->headers->has('Authorization')) {
+            return response()->json(['error' => 'Token não informado'], status: Response::HTTP_UNAUTHORIZED);
+        }
+
+        $auth = $request->headers->get('Authorization');
+        list($type, $tokenStr) = explode(' ', $auth, 2);
+        if ($type != 'Bearer') {
+            return response()->json(['error' => 'Token não informado'], status: Response::HTTP_UNAUTHORIZED);
+        }
+
+        try {
+            $token = $this->jwtService->validateToken($tokenStr);
+            if (!$token) {
+                return response()->json(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $uncrytedToken = $this->jwtService->parseToken($tokenStr);
+            $tokenClaims = $uncrytedToken->claims();
+
+            $userId = $tokenClaims->get('sub');
+            if (!$userId) {
+                return response()->json(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $user = User::find($userId);
+            if (is_null($user)) {
+                return response()->json(['error' => 'Usuário não encontrado'], status: Response::HTTP_UNAUTHORIZED);
+            }
+
+            $request->setUserResolver(fn () => $user);
+        } catch (\Throwable $e) {
+            return response()->json(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request);
+    }
+}
--- test-backend/app/Services/JwtService.php
+++ test-backend/app/Services/JwtService.php
@@ -6,6 +6,10 @@ use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use App\Models\User;
+use Lcobucci\JWT\Validation\Constraint\LooseValidAt;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\Clock\SystemClock;
+use Lcobucci\JWT\UnencryptedToken;
 
 class JwtService
 {
@@ -34,8 +38,37 @@ class JwtService
             ->toString();
     }
 
-    public function getConfiguration(): Configuration
+    private function getConfiguration(): Configuration
     {
         return $this->config;
+    }
+
+    private function getValidationConstraints(): array
+    {
+        $config = $this->getConfiguration();
+        return [
+            new SignedWith($config->signer(), $config->signingKey()),
+            new LooseValidAt(SystemClock::fromUTC()),
+        ];
+    }
+
+    public function validateToken(string $token): bool
+    {
+        try {
+            $config = $this->getConfiguration();
+            $parsedToken = $config->parser()->parse($token);
+            $constraints = $this->getValidationConstraints();
+            $validator = $this->config->validator();
+
+            return $validator->validate($parsedToken, ...$constraints);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function parseToken(string $token): UnencryptedToken
+    {
+        $config = $this->getConfiguration();
+        return $config->parser()->parse($token);
     }
 }
